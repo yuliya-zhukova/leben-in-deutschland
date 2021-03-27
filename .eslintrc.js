b19015bd6f713a2e25d63ffeda9c1794627c5bcb
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-03-27 10:29:18 +0100

    Restore .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.eslint.json'
+  },
+  ignorePatterns: ['build'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'jest/expect-expect': 'off'
+  }
+};
