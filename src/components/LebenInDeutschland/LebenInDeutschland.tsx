581ac18c616cfe57ab9a2403ee87a8a6394fcc59
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-03-31 12:38:34 +0200

    LID-5 Remove demo code

--- src/components/LebenInDeutschland/LebenInDeutschland.tsx
+++ src/components/LebenInDeutschland/LebenInDeutschland.tsx
@@ -1,5 +1,3 @@
-import { Badge } from 'primereact/badge';
-
 import { questions } from '../../data/questions';
 import { TestLebenInDeutschland } from '../TestLebenInDeutschland/TestLebenInDeutschland';
 
@@ -7,7 +5,6 @@ export function LebenInDeutschland(): JSX.Element {
   return (
     <div>
       <h1>Leben in Deutschland</h1>
-      <Badge value="42" />
 
       <TestLebenInDeutschland questions={questions} />
     </div>
