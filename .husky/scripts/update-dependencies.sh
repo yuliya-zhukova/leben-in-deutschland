ec9452b8373e1cb675bdba06871b64ff75e59eb7
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-04-02 16:36:29 +0200

    LID-16 Improve Git hooks' messages

--- .husky/scripts/update-dependencies.sh
+++ .husky/scripts/update-dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-printf "Updating dependencies...\n"
-
 # Based on https://gist.github.com/sindresorhus/7996717
 
+echo "Checking dependencies...\n"
+
 changedFiles="$(git diff-tree -r --name-only --no-commit-id HEAD@{1} HEAD)"
+echo "Changed files:"
+echo "$changedFiles\n"
 
 isChanged() {
 	echo "$changedFiles" | grep --quiet "$1" && return 0 || return 1
 }
 
-echo "$changedFiles"
-
-isChanged "package.json\|package-lock.json" && npm install || printf "Dependencies are up to date.\n"
+isChanged "package.json\|package-lock.json" && (echo "Updating dependencies..." && npm install) || echo "Dependencies are up to date.\n"
