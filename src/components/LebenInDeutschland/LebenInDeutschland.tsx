0e2121611edbe1536f31c3333fd1b5884794d913
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-04-05 16:26:50 +0200

    LID-15 Set up React router

--- src/components/LebenInDeutschland/LebenInDeutschland.tsx
+++ src/components/LebenInDeutschland/LebenInDeutschland.tsx
@@ -1,12 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 import { questions } from '../../data/questions';
 import { TestLebenInDeutschland } from '../TestLebenInDeutschland/TestLebenInDeutschland';
 
 export function LebenInDeutschland(): JSX.Element {
-  return (
-    <div>
-      <h1>Leben in Deutschland</h1>
+  const appRoot = process.env.PUBLIC_URL;
 
-      <TestLebenInDeutschland questions={questions} />
-    </div>
+  return (
+    <Router>
+      <div>
+        <h1>Leben in Deutschland</h1>
+
+        <Switch>
+          <Route path={`${appRoot}/lorem`}>
+            <h2>Lorem</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+              adipisci perferendis at quae illum, optio nam fugiat molestias
+              mollitia nihil corrupti, numquam laboriosam, soluta voluptatum
+              exercitationem illo id officia nobis.
+            </p>
+          </Route>
+          <Route exact path={appRoot}>
+            <TestLebenInDeutschland questions={questions} />
+          </Route>
+          <Redirect to={appRoot} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
