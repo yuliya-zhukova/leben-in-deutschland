8bbdc66f36067fba7e698f10fcf52a2fff7984e4
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-04-11 15:04:49 +0200

    LID-17 Update UI using PrimeReact components

--- src/components/TestStep/TestStep.tsx
+++ src/components/TestStep/TestStep.tsx
@@ -1,3 +1,5 @@
+import { Button } from 'primereact/button';
+
 import { Answer, Question } from '../../data/questions';
 
 import styles from './TestStep.module.css';
@@ -11,21 +13,22 @@ export function TestStep(props: TestStepProps): JSX.Element {
   const { question, onAnswer } = props;
 
   return (
-    <div data-cy="TestStep">
+    <div data-cy="TestStep" className={styles.TestStep}>
       <div className={styles.TestStepHeader}>
-        {question.id} — {question.text}
+        <h3>
+          <span className={styles.QuestionId}>{question.id}.&ensp;</span>
+          {question.text}
+        </h3>
       </div>
       {question.answers.map(
         (answer: Answer): JSX.Element => (
-          <button
-            type="button"
+          <Button
             key={answer.id}
-            className={styles.TestStepAnswerButton}
+            label={answer.text}
+            className={`p-button-rounded p-button-outlined ${styles.TestStepAnswerButton}`}
             onClick={() => onAnswer(answer.id)}
             data-cy="TestStep__answer"
-          >
-            {answer.text}
-          </button>
+          />
         )
       )}
     </div>
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,11 +1,33 @@
+import { Button } from 'primereact/button';
+
 import { Link } from 'react-router-dom';
+
+import styles from './Welcome.module.css';
 
 export function Welcome(): JSX.Element {
   return (
-    <div data-cy="Welcome">
-      <h1>Test &quot;Leben in Deutschland&quot;</h1>
-      <Link to="/test" data-cy="Welcome__start-test-link">
-        Start
+    <div data-cy="Welcome" className={`lid-container ${styles.Welcome}`}>
+      <h1 className={styles.title}>
+        Test
+        <br />
+        „Leben&nbsp;in&nbsp;Deutschland“
+      </h1>
+      <div className={styles.main}>
+        <p>
+          Answer 5 questions
+          <br />
+          to check your knowledge.
+        </p>
+      </div>
+      <Link
+        to="/test"
+        data-cy="Welcome__start-test-link"
+        className="lid-buttonAsLink"
+      >
+        <Button
+          label="Start"
+          className={`p-button-rounded ${styles.startButton}`}
+        />
       </Link>
     </div>
   );
