cf523339301adaff1f01e1507b9032a2c2c7bb8a
Maxim Zhukov <zhkv.mxm@gmail.com>
2021-04-18 12:56:41 +0200

    LID-19 Extract navigation into a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Nav />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Nav from "@/components/Nav.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Nav,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
